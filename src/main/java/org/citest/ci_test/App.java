812d22e01aa567e7b4dc5b2f3192c06c89b9962e
Igor Lazebny <igor.lazebny@gmail.com>
2010-12-15 17:29:12 +0200

    added dummy cless and test

---
+++ src/main/java/org/citest/ci_test/App.java
@@ -0,0 +1,23 @@
+package org.citest.ci_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static double squear (double arg)
+    {
+    	return arg * arg;
+    }
+
+    public static double sum (double arg)
+    {
+    	return arg + arg;
+    }
+}
---
+++ src/test/java/org/citest/ci_test/AppTest.java
@@ -0,0 +1,44 @@
+package org.citest.ci_test;
+
+import org.citest.ci_test.App;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    public void testSquear ()
+    {
+    	double result = App.squear(2);
+    	assertEquals(4.0, result);
+    }
+
+    public void testSum ()
+    {
+    	double result = App.sum(2);
+    	assertEquals(4.0, result);
+    }
+}
